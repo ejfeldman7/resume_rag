b54d7b8126c8e510e4fd6d0f3b0006b0361e040a
Ethan Feldman <ejfeldman7@gmail.com>
2024-06-17 20:26:40 -0700

    Align embedding dimensions

--- utils/chatbot.py
+++ utils/chatbot.py
@@ -3,6 +3,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from utils.embeddings import EmbeddingRetriever
 from transformers import T5Tokenizer, T5ForConditionalGeneration  # noqa
 from utils.pdf_extract import PDFExtractor
+from sklearn.decomposition import PCA
 
 
 class Chatbot:
@@ -17,14 +18,21 @@ class Chatbot:
 
     def chunk_document(self):
         chunks = []
-        for i in range(0, len(self.pdf_text), self.chunk_size):
-            chunk = self.pdf_text[i:i + self.chunk_size]
-            chunks.append(chunk)
+        start_idx = 0
+        while start_idx < len(self.pdf_text):
+            end_idx = start_idx + self.chunk_size
+            if end_idx >= len(self.pdf_text):
+                end_idx = len(self.pdf_text)
+            chunks.append(self.pdf_text[start_idx:end_idx])
+            start_idx = end_idx
         return chunks
 
     def retrieve_passages(self, query):
         query_embedding = self.embedding_retriever.get_embedding(query)
-        scores = cosine_similarity(query_embedding, self.corpus_embeddings)[0]
+        pca = PCA(n_components=self.corpus_embeddings.shape[1])
+        query_embedding_reduced = pca.fit_transform(query_embedding)
+
+        scores = cosine_similarity(query_embedding_reduced, self.corpus_embeddings)[0]
         best_idx = scores.argmax()
         relevant_chunk = self.chunk_document()[best_idx]
         return relevant_chunk
