382ca3d067d47a2550fc9fbef2a7c1b12bc367e4
Ethan Feldman <ejfeldman7@gmail.com>
2024-06-17 18:02:07 -0700

    Use AutoModelForCausalLM

--- utils/chatbot.py
+++ utils/chatbot.py
@@ -1,10 +1,10 @@
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
 class Chatbot:
     def __init__(self):
         self.tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
-        self.model = AutoModelForSeq2SeqLM.from_pretrained("distilgpt2")
+        self.model = AutoModelForCausalLM.from_pretrained("distilgpt2")
 
     def generate_response(self, passage, query):
         inputs = self.tokenizer([passage + " " + query], return_tensors="pt")
