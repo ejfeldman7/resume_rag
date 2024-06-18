285061b45a7c91ccaae3093d0b860077587223ca
Ethan Feldman <ejfeldman7@gmail.com>
2024-06-17 18:10:38 -0700

    Update chatbot

--- utils/chatbot.py
+++ utils/chatbot.py
@@ -1,12 +1,15 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-
 class Chatbot:
     def __init__(self):
         self.tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
         self.model = AutoModelForCausalLM.from_pretrained("distilgpt2")
 
     def generate_response(self, passage, query):
-        inputs = self.tokenizer([passage + " " + query], return_tensors="pt")
-        outputs = self.model.generate(**inputs)
-        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        input_text = passage + " " + query
+        input_ids = self.tokenizer.encode(input_text, return_tensors="pt")
+
+        output = self.model.generate(input_ids, max_length=150, num_return_sequences=3, do_sample=True)
+        responses = [self.tokenizer.decode(output_sequence, skip_special_tokens=True) for output_sequence in output]
+
+        return responses
